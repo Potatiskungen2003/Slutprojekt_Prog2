dd40a69ae226a0bbe8f1d42c7c7f1826d5431ac1
Edward <potatiskungen03@gmail.com>
2022-04-21 15:09:58 +0200

    started working on passwords and styff

--- Slutprojekt.py
+++ Slutprojekt.py
@@ -1,18 +1,31 @@
 from getpwd import getpwd
 
+""" I admin classen vill du ha alla metoder som ska gå att använda
+I aludt har du några färre typ att kolla andras lösenor eller liknande 
+I underage ska man nästan inte ha några ärvda metoder förutom dem som visar saker """
 class Bank:
+    def __init__(self, name, password, balance):
+        self.name = name
+        self.__password = password
+        self.balance = balance
+    
+    def login():
+        pass
+        
+
+class Admin(Bank):
     pass
 
-class Admin:
+class Adult(Bank):
     pass
 
-class Adult:
-    pass
-
-class Underage:
+class Underage(Bank):
     pass
 
 
+anvandare = Bank('jeff', 'jeeeeeef', '34567890')
+
+print(anvandare._password)
 
 secret = getpwd()
 
